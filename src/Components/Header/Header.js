a8c82f2d191e33f44eece768ee9a55eac300ce67
Dave Tyson <davetysonbass@gmail.com>
2023-06-24 14:58:24 -0400

    added font awesome, and a few header items

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,23 @@
-import Menu from './Menu';
+// import Menu from './Menu';
+import hurstNewLogo from '../../assets/hurstNewLogo.png';
 
 const Header = () => {
     return(
-        <>
+        <header>
             <h1>Header</h1>
-            <Menu />
-        </>
+            <button>
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <span class="sr-only">Menu</span>
+            </button>
+            <figure>
+                <img src={hurstNewLogo} alt="Tap On Cards store logo" />
+            </figure>
+            <div>
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="sr-only">Cart Page</span>
+                <p>1</p>
+            </div>
+            {/* <Menu /> */}
+        </header>
     );
 };
 
