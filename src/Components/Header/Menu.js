bee3aa7196c68165c1424bdd2921cb925a7fff23
Dave Tyson <davetysonbass@gmail.com>
2023-06-24 15:52:47 -0400

    a few small menu tweaks

--- src/Components/Header/Menu.js
+++ src/Components/Header/Menu.js
@@ -1,6 +1,16 @@
+import '../../styling/header.css';
+
 const Menu = () => {
     return(
-        <h1>Menu</h1>
+        <section className="menu">
+            <ul>
+                <li>Menu link 1</li>
+                <li>Menu link 2</li>
+                <li>Menu link 3</li>
+                <li>Menu link 4</li>
+                <li>Menu link 5</li>
+            </ul>
+        </section>
     );
 };
 
